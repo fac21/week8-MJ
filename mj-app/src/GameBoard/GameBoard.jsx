2551282ef892a87b0e19a51f6bd56a0470a18e17
Mariya Peychinova <mariapeichinova@gmail.com>
2021-05-13 14:47:18 +0100

    add font-family and kill the ghost
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>

--- mj-app/src/GameBoard/GameBoard.jsx
+++ mj-app/src/GameBoard/GameBoard.jsx
@@ -5,16 +5,29 @@ function GameBoard(props) {
    
     React.useEffect(() => {
         const ghostContainer = document.querySelector('.game-board');
-
-        for (let i = 0; i < 10; i++) {
+        const ghostArray = new Array(10);
+        ghostArray.fill('ghost');
+        console.log(ghostArray);
+        for (let i = 0; i < ghostArray.length; i++) {
             const element = document.createElement('div')
             element.style.top=`${Math.random()*100}%`
             element.style.left=`${Math.random()*100}%`
-            element.style.animation = `twinkle ${1+Math.random()*20}s ease infinite`
+            element.style.animation = `twinkle ${1+Math.random()*15}s ease infinite`
             ghostContainer.appendChild(element)
             element.setAttribute('class', 'ghost')
         }
+        const ghosts = document.querySelectorAll('.ghost');
+        function killGhost(event){
 
+            if(!event.isTrusted) return;
+        
+            //score++;
+            this.classList.add('hidden');
+            //scoreBoard.textContent = score;
+        }
+        
+        ghosts.forEach(ghost => ghost.addEventListener('click', killGhost));
+        
     }, []);
     return (
         <div className="game-board"></div>
